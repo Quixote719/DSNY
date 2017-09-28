976b8a6c1167c403e45905bad13387856fcbd3d9
Daniel Wang <dwang@dsny.nyc.gov>
2017-09-28 09:58:41 -0400

    test for name changing

--- src/components/shared/footer/footer.js
+++ src/components/shared/footer/footer.js
@@ -11,8 +11,8 @@ class Footer extends Component{
 
     render() {
       const Service = [{name:'Collection',link:'Cpage'},{name:'Cleaning',link:'CL'},{name:'Snow',link:'Spage'}];
-      const App = [{name:'DSNY Info',link:'https://medium.com/',AppStore:'https://itunes.apple.com/us/app/dsny-info/id1139303678?mt=8',GooglePlay:'https://play.google.com/store/apps/details?id=gov.nyc.dsny.dsny&hl=en'},
-      {name:'donateNYC',link:'https://medium.com/',AppStore:'https://itunes.apple.com/us/app/donatenyc/id1112567037?mt=8',GooglePlay:'https://play.google.com/store/apps/details?id=gov.nyc.dsny.donate&hl=en'},
+      const App = [{name:'DSNY Info',link:'https://www.google.com/',AppStore:'https://itunes.apple.com/us/app/dsny-info/id1139303678?mt=8',GooglePlay:'https://play.google.com/store/apps/details?id=gov.nyc.dsny.dsny&hl=en'},
+      {name:'donateNYC',link:'https://www.google.com/',AppStore:'https://itunes.apple.com/us/app/donatenyc/id1112567037?mt=8',GooglePlay:'https://play.google.com/store/apps/details?id=gov.nyc.dsny.donate&hl=en'},
       {name:'Zero Waste School',link:'https://medium.com/',AppStore:'',GooglePlay:''}];
       const Archive = [{name:'Newsletters',link:'l1'},
                        {name:'Memoranda of Understanding',link:'l2'},
