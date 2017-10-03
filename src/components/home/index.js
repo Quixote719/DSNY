f0cd09dda0fd99b344a56af1c4164d0c09c63139
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-03 17:14:37 -0400

    merge conflit resolved

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import { Carousel } from 'react-bootstrap';
+import {Carousel} from 'react-bootstrap';
 import * as actions from '../../actions/actions_home';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import month from 'month';
 import styles from '../../content/styles/home.css';
-import { Grid, Row, Col } from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 import SubSectionButton from '../shared/sub_section_button';
 import EventList from './Events/event_list';
@@ -20,7 +20,7 @@ class Home extends Component {
     var monthCarouselPanel = month('MMM').toUpperCase();
     this.state = {
       dateCarouselPanel: dateCarouselPanel,
-      monthCarouselPanel: monthCarouselPanel,
+      monthCarouselPanel: monthCarouselPanel
     };
   }
   componentWillMount() {
@@ -35,10 +35,10 @@ class Home extends Component {
     console.log("Panel Values: ")
     console.log(this.props.carouselPanelItems)
 
-    var carouselDataItemList = this.props.carouselItemsBigData.map(function (item, index) {
+    var carouselDataItemList = this.props.carouselItemsBigData.map(function(item, index) {
       return (
         <Carousel.Item key={index}>
-          <img src={item.heroImage} />
+          <img src={item.heroImage}/>
           <Carousel.Caption>
             {item.heroTitle}
           </Carousel.Caption>
@@ -46,12 +46,12 @@ class Home extends Component {
       );
     }, this)
 
-    var carouselPanelDataItemList = this.props.carouselPanelItems.map(function (items) {
-      return items.map(function (item) {
+    var carouselPanelDataItemList = this.props.carouselPanelItems.map(function(items) {
+      return items.map(function(item) {
         return (
           <div className="panelData">
             <div className="border-top"></div>
-            <div className="programImage"><img src={item.panelItemIcon} /></div>
+            <div className="programImage"><img src={item.panelItemIcon}/></div>
             <div className="detailsProgram">{item.panelItemType}</div>
             <div className="statusProgram">{item.panelItemStatus}</div>
           </div>
@@ -59,7 +59,7 @@ class Home extends Component {
       })
     }, this)
 
-    var mobileProgramList = this.props.programListData.map(function (item, index) {
+    var mobileProgramList = this.props.programListData.map(function(item, index) {
       return (
         <div className="programMobileData" key={index}>
           <div className="programMobileOuter">
@@ -67,38 +67,37 @@ class Home extends Component {
               {item.programTitle}
             </span>
             <span className="programMobileImageSpan">
-              <img src={item.programImage} className="programMobileImage" />
+              <img src={item.programImage} className="programMobileImage"/>
             </span>
           </div>
         </div>
       );
     }, this)
 
-    var programsList = this.props.programListData.map(function (item, index) {
+    var programsList = this.props.programListData.map(function(item, index) {
       if (index == 0) {
         return (
           <div key={index}>
-            <Col className="programData serviceRequestCol"  >
+            <Col className="programData serviceRequestCol">
               <Col xs={12} lg={2} id="programCol">
                 <div className="serviceRequestParent">
                   <div className="serviceRequestTitle">
                     Check Service Request Number
-                    </div>
+                  </div>
                   <div className="serviceRequestInput">
-                    <input className="serviceRequestSearch" type="text" placeholder="Service Request Number" >
-                    </input>
+                    <input className="serviceRequestSearch" type="text" placeholder="Service Request Number"></input>
                   </div>
                   <div className="searchProgramBtnDiv">
-                    <SubSectionButton title='SEARCH' />
+                    <SubSectionButton title='SEARCH'/>
                   </div>
                 </div>
               </Col>
             </Col>
-            <Col className="programData" >
+            <Col className="programData">
               <Col xs={12} lg={2} id="programCol">
                 <div>
                   <div className="programImageDiv">
-                    <img src={item.programImage} />
+                    <img src={item.programImage}/>
                   </div>
                   <div className="programTitleDiv">
                     {item.programTitle}
@@ -114,7 +113,7 @@ class Home extends Component {
           <Col xs={12} lg={2} id="programCol">
             <div>
               <div className="programImageDiv">
-                <img src={item.programImage} />
+                <img src={item.programImage}/>
               </div>
               <div className="programTitleDiv">
                 {item.programTitle}
@@ -133,7 +132,7 @@ class Home extends Component {
               <ul className="carouselLeftPanel">
                 <li className="statusTitle">
                   TODAY
-              </li>
+                </li>
                 <li className="dateMonthCarousel">
                   <div className="dateMonthCarousel">
                     <div className="dateCarouselPanel">
@@ -151,11 +150,7 @@ class Home extends Component {
                   <button className="panelButton">More</button>
                 </li>
               </ul>
-              <Carousel className="newsEventsCarousel" prevIcon={<span className="newsEventsCarouselIcon">
-                <img src='http://www1.nyc.gov/assets/home/images/global/heroleft5.svg' alt="carouselleftArrow" />
-              </span>} nextIcon={<span className="newsEventsCarouselIcon">
-                <img src='http://www1.nyc.gov/assets/home/images/global/heroright5.svg' alt="carouselRightArrow" />
-              </span>}>
+              <Carousel className="newsEventsCarousel" prevIcon={< span className = "newsEventsCarouselIcon" > <img src='http://www1.nyc.gov/assets/home/images/global/heroleft5.svg' alt="carouselleftArrow"/> < /span>} nextIcon={< span className = "newsEventsCarouselIcon" > <img src='http://www1.nyc.gov/assets/home/images/global/heroright5.svg' alt="carouselRightArrow"/> < /span>}>
                 {carouselDataItemList}
               </Carousel>
             </div>
@@ -167,26 +162,28 @@ class Home extends Component {
             <Col xs={12} md={6} className="searchRidParent">
               <div id="TextureSquare">
                 <div id="innersquare">
-                  <input className="ridOfSearch" type="text" placeholder="How to get rid of ..." >
-                  </input>
+                  <input className="ridOfSearch" type="text" placeholder="How to get rid of ..."></input>
                   <i className="fa fa-search ridSearch" id="ridSearch"></i>
-                  <div className="exampleRidSearch"> Example: battery, mattress, TVs </div>
+                  <div className="exampleRidSearch">
+                    Example: battery, mattress, TVs
+                  </div>
                 </div>
               </div>
             </Col>
             <Col xs={12} md={6} className="searchCollectionParent">
               <div id="TextureSquare">
                 <div id="innersquare">
-                  <input className="ridOfSearch" type="text" placeholder="When is Collection at ..." >
-                  </input>
+                  <input className="ridOfSearch" type="text" placeholder="When is Collection at ..."></input>
                   <i className="fa fa-search collectionSearch" id="collectionSearch"></i>
-                  <div className="exampleRidSearch"> Example: 454 W 12th Ave, New York </div>
+                  <div className="exampleRidSearch">
+                    Example: 454 W 12th Ave, New York
+                  </div>
                 </div>
               </div>
             </Col>
           </Row>
         </div>
-<<<<<<< HEAD
+
         <div className="programContainerParent programContainerParentTablet">
           <div className="programContainer">
             <div className="serviceRequestParentTablet">
@@ -194,9 +191,8 @@ class Home extends Component {
                 Check Service Request Number
               </div>
               <div className="serviceRequestInputTablet">
-                <input className="serviceRequestSearchTablet" type="text" placeholder="Service Request Number" >
-                </input>
-                <SubSectionButton title='SEARCH' />
+                <input className="serviceRequestSearchTablet" type="text" placeholder="Service Request Number"></input>
+                <SubSectionButton title='SEARCH'/>
               </div>
             </div>
             {mobileProgramList}
@@ -206,28 +202,23 @@ class Home extends Component {
           </div>
         </div>
 
-=======
         <div className="container">
-            <EventList/>
+          <EventList/>
         </div>
->>>>>>> dev
+
       </div>
     )
   }
 }
 
 function mapStateToProps(state) {
-  return {
-    carouselItemsBigData: state.carouselDataReducer.carouselItems,
-    carouselPanelItems: state.carouselDataReducer.carouselPanelItemsTemporary,
-    programListData: state.carouselDataReducer.programListData,
-  }
+  return {carouselItemsBigData: state.carouselDataReducer.carouselItems, carouselPanelItems: state.carouselDataReducer.carouselPanelItemsTemporary, programListData: state.carouselDataReducer.programListData}
 }
 
 let actionList = {
   carouselData: actions.carouselData,
   carouselPanelDataTemporary: actions.carouselPanelDataTemporary,
-  programData: actions.programData,
+  programData: actions.programData
 };
 
 Home = connect(mapStateToProps, actionList)(Home);
