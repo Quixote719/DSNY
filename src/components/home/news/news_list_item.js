f2a30d3dbf3a1e0133a4ee559b12101bccc6e722
Dinesh Doshi <ddoshi@dsny.nyc.gov>
2017-10-09 17:34:40 -0400

    Mobile display changes

--- src/components/home/news/news_list_item.js
+++ src/components/home/news/news_list_item.js
@@ -1,5 +1,4 @@
 import React, {Component} from "react";
-import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {Grid, Row, Col, Clearfix} from 'react-bootstrap';
 import styles from '../../../content/styles/newsListItem.css';
@@ -7,54 +6,96 @@ import moment from 'moment';
 
 class NewsListItem extends Component {
 
-  render() {
-   if (this.props.itemCounter == 0) {
-    return (
-      <Col xs={12} sm={6} md={6} className='alignright newsSection'>
-        <div className='newsSectiondDefaultcardTI'>
-          <div className='defaultcardImage' style={{
-            backgroundImage: `url(${this.props.image})`
-          }}></div>
-          {/*<div className='cardTitle'>
-            <div className='cardTitleText'>{this.props.title}</div>
-          </div>*/}
-        </div>
-      </Col>
-    );
-   }
-   else if (this.props.itemCounter == 99) {
-    return (
-      <Col xs={12} sm={6} md={6} className='alignright newsSection'>
-        <div className='newsSectioncardTI'>
-          {/*<div className='defaultcardImage' style={{
-            backgroundImage: `url(${this.props.image})`
-          }}></div>*/}
-          <div className='defaultcardImage newscardTitle'>
-            <div className='newscardTitleDate'>{moment(this.props.date).format('MMMM D, YYYY')}</div>
-            <div className='newsDefaultcardTitleText'>{this.props.title}</div>
-            <div className='newscardDescription'>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes.</div>
-          </div>
-        </div>
-      </Col>
-    );
-   }
-   else
-   {
-    return (
-      <Col xs={12} sm={6} md={3} className='newsSection'>
-        <div className='newsSectioncardTI'>
-          <div className='newscardImage' style={{
-            backgroundImage: `url(${this.props.image})`
-          }}></div>
-          <div className='newscardTitle'>
-            <div className='newscardTitleDate'>{moment(this.props.date).format('MMMM D, YYYY')}</div>
-            <div className='newscardTitleText'>{this.props.title}</div>
-          </div>
-        </div>
-      </Col>
-    );
-   }
-  };
-};
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <div>
+                {this.NewsList()}
+                {this.mobileNewsList()}
+            </div>
+        );
+    }
+
+  
+    //To display News details on Desktop and Tablet Landscape
+    NewsList()
+    {
+        //To display Top News Image on Desktop and IPAD Landscape .  alignright class is aligning this card to the right
+        if (this.props.itemCounter == 0) {
+            return (
+            <Col xs={12} sm={6} md={6} className='alignright newsDesktopSection'>
+                <div className='newsSectiondDefaultcardTI'>
+                <div className='defaultcardImage' style={{
+                    backgroundImage: `url(${this.props.image})`
+                }}></div>
+                </div>
+            </Col>
+            );
+        }
+        //To display Top News Details with Title & Description on Desktop and IPAD Landscape
+        else if (this.props.itemCounter == 99) {
+            return (
+            <Col xs={12} sm={6} md={6} className='alignright newsDesktopSection'>
+                <div className='newsSectioncardTI'>
+                <div className='defaultcardImage newscardTitle'>
+                    <div className='newscardTitleDate'>{moment(this.props.date).format('MMMM D, YYYY')}</div>
+                    <div className='newsDefaultcardTitleText'>{this.props.title}</div>
+                    <div className='newscardDescription'>{this.props.description}</div>
+                </div>
+                </div>
+            </Col>
+            );
+        }
+        //To display remaining News Details with Image & Title on Desktop and IPAD Landscape
+        else
+        {
+            return (
+            <Col xs={12} sm={6} md={3} className='newsDesktopSection'>
+                <div className='newsSectioncardTI'>
+                <div className='newscardImage' style={{
+                    backgroundImage: `url(${this.props.image})`
+                }}></div>
+                <div className='newscardTitle'>
+                    <div className='newscardTitleDate'>{moment(this.props.date).format('MMMM D, YYYY')}</div>
+                    <div className='newscardTitleText'>{this.props.title}</div>
+                </div>
+                </div>
+            </Col>
+            );
+        }
+    }
+
+    //To display News details on Tablet Portrait and Mobile Devices
+    mobileNewsList()
+    {
+        if (this.props.itemCounter != 99) // This is to ignore the Top row description card that is displayed on Desktop
+        {
+            return (
+                <div>
+                    <Col xs={5} sm={5} md={5} className='newsMobileSection'>
+                        <div className='newsSectioncardTI'>
+                        <div className='newscardImage' style={{
+                            backgroundImage: `url(${this.props.image})`
+                        }}></div>
+                        </div>
+                    </Col>
+                    <Col xs={7} sm={7} md={7} className='newsMobileSection'>
+                        <div className='newsSectioncardTI'>
+                        <div className='newscardTitle'>
+                            <div className='newscardTitleDate'>{moment(this.props.date).format('MMMM D, YYYY')}</div>
+                            <div className='newscardTitleText'>{this.props.title}</div>
+                        </div>
+                        </div>
+                    </Col>
+                </div>
+            );
+        }
+ 
+    }
+
+}
 
 export default NewsListItem;
