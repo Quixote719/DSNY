db106f36a8861d20de89f7d0d057ba9bbd6c2f3e
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-12 16:49:11 -0400

    modular dropdown component

--- src/components/shared/Sub_section_dropdown.js
+++ src/components/shared/Sub_section_dropdown.js
@@ -1,9 +1,11 @@
+import _ from "lodash";
 import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import PropTypes from 'prop-types';
 import styles from '../../content/styles/SubSectionDropdown.css';
 import {Dropdown, MenuItem, Button, DropdownButton} from 'react-bootstrap';
+import {dropDownList} from "../../actions";
 import '../../../node_modules/font-awesome/css/font-awesome.min.css';
 
 class SubSectionDropdown extends Component {
@@ -16,15 +18,29 @@ class SubSectionDropdown extends Component {
     this.props.ondropDownChange(term);
   }
 
+  componentDidMount() {
+    const {category} = this.props;
+    this.props.dropDownList(category);
+  }
+
+  renderList(List) {
+    return _.map(List, Item => {
+      return (
+        <MenuItem key={Item} className='SubSectionDropdownMenuItem' onSelect={event => this.onInputChange(event)} eventKey={Item}>{Item}</MenuItem>
+      );
+    });
+  }
+
   render() {
+    const {List} = this.props;
+    console.log(List);
     return (
       <div>
         <div className='SubSectionDropdown'>
           <DropdownButton className='dropDownButtonText' bsStyle="default" title={< div className = "dropDownTitle" > <div className="col-xs-10 dropDownSubTitle">
             {this.props.selectedOption}
-          </div> < div className = "col-xs-2 downArrow" > <i className="fa fa-chevron-down chevron-down-font-awesome"></i></div></div>} noCaret id="dropdown-no-caret">
-            <MenuItem className='SubSectionDropdownMenuItem' onSelect={event => this.onInputChange(event)} eventKey="2017">2017</MenuItem>
-            <MenuItem className='SubSectionDropdownMenuItem' onSelect={event => this.onInputChange(event)} eventKey="2016">2016</MenuItem>
+          </div> < div className = "col-xs-2 downArrow" > <i className="fa fa-chevron-down chevron-down-font-awesome"></i> < /div> < /div >} noCaret id="dropdown-no-caret">
+            {this.renderList(List)}
           </DropdownButton>
           <div className='hairlineGreen'></div>
         </div>
@@ -35,7 +51,12 @@ class SubSectionDropdown extends Component {
 
 SubSectionDropdown.propTypes = {
   onClick: PropTypes.func,
-  selectedOption: PropTypes.string
+  selectedOption: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  category: PropTypes.string.isRequired
 };
 
-export default SubSectionDropdown;
+function mapStateToProps(state) {
+  return {List: state.dropdownList};
+}
+
+export default connect(mapStateToProps, {dropDownList})(SubSectionDropdown);
