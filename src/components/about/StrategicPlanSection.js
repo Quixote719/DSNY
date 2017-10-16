743e1745403f37e26fe9be51358e0568fe178b19
Daniel Wang <dwang@dsny.nyc.gov>
2017-10-16 11:35:25 -0400

    link functionality of title cards

--- src/components/about/StrategicPlanSection.js
+++ src/components/about/StrategicPlanSection.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import SubSectionHeader from '../shared/sub_section_header';
 import SubSectionButton from '../shared/sub_section_button';
 
@@ -9,7 +10,9 @@ class StrategicPlanSection extends Component {
       <div className="StrategicPlan">
         <SubSectionHeader title = {this.props.StrategicPlanProps.title}/>
         <div dangerouslySetInnerHTML={{__html: this.props.StrategicPlanProps.content}}></div>
-        <SubSectionButton title='SEE STRATEGIC PLAN'/>
+        <Link to="/about/strategicplan">
+          <SubSectionButton title='SEE STRATEGIC PLAN'/>
+        </Link>
       </div>
     )
   }
