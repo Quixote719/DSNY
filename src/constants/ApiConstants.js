b16876721dca753df10b28747e0811e6a612ffe8
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-19 14:02:29 -0400

    press release list

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -13,7 +13,7 @@ export const MEDIAS_ALL_URL = `${ROOT_WORDPRESS_URL}wp/v2/media/`;
 export const MEDIA_URL = `${ROOT_WORDPRESS_URL}wp/v2/media/id`;
 export const SUB_SECTION_HEADER_URL = constructWpUrl('wp/v2/pagesection?slug=id');
 export const FETCH_PRESS_RELEASE_DETAILS_URL = constructWpUrl('dsny/v1/getPressReleasePage?name=id');
-export const FETCH_PRESS_RELEASE_LIST_URL = constructWpUrl('dsny/v1/getPageData?name=press-releases');
+export const FETCH_PRESS_RELEASE_LIST_URL = constructWpUrl('dsny/v1/getPressReleaseListPage?year=:Year');
 export const FETCH_LANDING_PAGE_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData?name=:category`;
 export const FETCH_CARD_DETAILS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData?name=:category`;
 export const FETCH_DROPDOWN_LIST = `${ROOT_WORDPRESS_URL}dsny/v1/getDropdownOptions?cat=:category`;
