f09d1a18a979ed905cca799cabcd6b21f433ae3b
Daniel Wang <dwang@dsny.nyc.gov>
2017-10-20 16:33:31 -0400

    location page google map test

--- src/components/about/Location/index.js
+++ src/components/about/Location/index.js
@@ -1,7 +1,11 @@
 import React , { Component } from "react"
 import { compose, withProps } from "recompose"
 import { withScriptjs, withGoogleMap, GoogleMap, Marker  } from "react-google-maps"
-import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel";
+import { connect } from 'react-redux'
+import * as actions from '../../../actions/actions_about'
+import MarkerWithLabel from "react-google-maps/lib/components/addons/MarkerWithLabel"
+
+const google = window.google;
 const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDhdvaSCBpX16gKXvcdUx29_k1M9WAk8jo&v=3.exp&libraries=geometry,drawing,places",
@@ -14,23 +18,21 @@ const MyMapComponent = compose(
 )((props) =>
   <GoogleMap defaultZoom={14} defaultCenter={{ lat: 40.72046126, lng: -73.98974419 }}>
     <Marker position={{ lat: 40.72046126, lng: -73.98974419 }} onClick={()=>props.onMarkerClick({ lat: 40.72046126, lng: -73.98974419 })} />
-    <Marker position={{ lat: 40.72940126, lng: -73.98070419 }} onClick={()=>props.onMarkerClick} />
-    <Marker position={{ lat: 40.72540126, lng: -73.98570419 }} onClick={()=>props.onMarkerClick} />
+    <Marker icon={{ url: 'https://maxcdn.icons8.com/Share/icon/Maps//marker1600.png',scaledSize: new google.maps.Size(64,64) }} position={{ lat: 40.72940126, lng: -73.98070419 }} onClick={()=>props.onMarkerClick} />
+    <Marker icon={{ url: '../../../content/images/Map_marker_default.png' }} position={{ lat: 40.72540126, lng: -73.98570419 }} onClick={()=>props.onMarkerClick} />
     <Marker position={{ lat: 40.72840126, lng: -73.98170419 }} onClick={()=>props.onMarkerClick} />
     <Marker position={{ lat: 40.72040126, lng: -73.98370419 }} onClick={()=>props.onMarkerClick} />
     <Marker position={{ lat: 40.72340126, lng: -73.98770419 }} onClick={()=>props.onMarkerClick} />
-
-    {props.isMarkerShown && <MarkerWithLabel
-      position={{ lat: -34.397, lng: 150.644 }}
-
-      labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
-    >
-  <div>Hello There!</div>
-</MarkerWithLabel>}
+    <MarkerWithLabel
+      position = {{ lat: 40.72340126, lng: -73.98770419 }}
+      labelAnchor = {new google.maps.Point(0, 0)}
+      labelStyle = {{backgroundColor: "#FFFFFF", fontSize: "17px", padding: "7px"}}>
+      <div>Info</div>
+    </MarkerWithLabel>
   </GoogleMap>
 )
 
-class MyFancyComponent extends Component {
+class Location extends Component {
   state = {
     isMarkerShown: false,
   }
@@ -47,8 +49,6 @@ class MyFancyComponent extends Component {
 
   handleMarkerClick = (obj) => {
     alert(obj.lat);
-    // this.setState({ isMarkerShown: false })
-    // this.delayedShowMarker(obj)
   }
 
   render() {
@@ -61,4 +61,16 @@ class MyFancyComponent extends Component {
   }
 }
 
-export default MyFancyComponent
+function mapStateToProps(state) {
+  return {
+    LocationList: state.AboutDataReducer.LocationList,
+  }
+}
+
+let actionList = {
+  Location: actions.fetchLocationList,
+};
+
+Location = connect(mapStateToProps, actionList)(Location);
+
+export default Location
