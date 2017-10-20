9d6599b13f54cb765fefbfe464e9eb366f512d18
Daniel Wang <dwang@dsny.nyc.gov>
2017-10-20 12:59:21 -0400

    push to switch

--- src/components/about/Location/index.js
+++ src/components/about/Location/index.js
@@ -6,20 +6,27 @@ const MyMapComponent = compose(
   withProps({
     googleMapURL: "https://maps.googleapis.com/maps/api/js?key=AIzaSyDhdvaSCBpX16gKXvcdUx29_k1M9WAk8jo&v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: <div style={{ height: `450px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
   }),
   withScriptjs,
   withGoogleMap
 )((props) =>
   <GoogleMap defaultZoom={14} defaultCenter={{ lat: 40.72046126, lng: -73.98974419 }}>
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72046126, lng: -73.98974419 }} onClick={props.onMarkerClick} />}
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72940126, lng: -73.98070419 }} onClick={props.onMarkerClick} />}
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72540126, lng: -73.98570419 }} onClick={props.onMarkerClick} />}
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72840126, lng: -73.98170419 }} onClick={props.onMarkerClick} />}
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72040126, lng: -73.98370419 }} onClick={props.onMarkerClick} />}
-    {  props.isMarkerShown && <Marker position={{ lat: 40.72340126, lng: -73.98770419 }} onClick={props.onMarkerClick} />}
+    <Marker position={{ lat: 40.72046126, lng: -73.98974419 }} onClick={()=>props.onMarkerClick({ lat: 40.72046126, lng: -73.98974419 })} />
+    <Marker position={{ lat: 40.72940126, lng: -73.98070419 }} onClick={()=>props.onMarkerClick} />
+    <Marker position={{ lat: 40.72540126, lng: -73.98570419 }} onClick={()=>props.onMarkerClick} />
+    <Marker position={{ lat: 40.72840126, lng: -73.98170419 }} onClick={()=>props.onMarkerClick} />
+    <Marker position={{ lat: 40.72040126, lng: -73.98370419 }} onClick={()=>props.onMarkerClick} />
+    <Marker position={{ lat: 40.72340126, lng: -73.98770419 }} onClick={()=>props.onMarkerClick} />
 
+    {props.isMarkerShown && <MarkerWithLabel
+      position={{ lat: -34.397, lng: 150.644 }}
+
+      labelStyle={{backgroundColor: "yellow", fontSize: "32px", padding: "16px"}}
+    >
+  <div>Hello There!</div>
+</MarkerWithLabel>}
   </GoogleMap>
 )
 
@@ -32,15 +39,16 @@ class MyFancyComponent extends Component {
     this.delayedShowMarker()
   }
 
-  delayedShowMarker = () => {
+  delayedShowMarker = (obj) => {
     setTimeout(() => {
       this.setState({ isMarkerShown: true })
-    }, 3000)
+    }, 1000)
   }
 
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false })
-    this.delayedShowMarker()
+  handleMarkerClick = (obj) => {
+    alert(obj.lat);
+    // this.setState({ isMarkerShown: false })
+    // this.delayedShowMarker(obj)
   }
 
   render() {
