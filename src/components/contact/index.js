88c44307fccfc9f7cb0006ba961aae6bf8a72f2d
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-23 11:35:55 -0400

    NPM Module: "react-jsonschema-form": "^0.51.0", test component for form (DSNYWEB3-444)

---
+++ src/components/contact/index.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import Form from "react-jsonschema-form";
+
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {
+      type: "string",
+      title: "Title",
+      default: "A new task"
+    },
+    done: {
+      type: "boolean",
+      title: "Done?",
+      default: false
+    }
+  }
+};
+
+const log = (type) => console.log.bind(console, type);
+
+class TestForm extends Component {
+
+  render() {
+
+    return (<Form schema={schema} onChange={log("changed")} onSubmit={log("submitted")} onError={log("errors")}/>);
+  };
+};
+
+export default TestForm;
