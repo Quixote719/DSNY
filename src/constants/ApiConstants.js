5cc06935a5df59528f64d7df76c41dd90902ff05
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-27 10:56:40 -0400

    url updated

--- src/constants/ApiConstants.js
+++ src/constants/ApiConstants.js
@@ -9,8 +9,8 @@ export const WORDPRESS_ROOT_URL = `${ROOT_WORDPRESS_URL}`;
 export const HOME_PAGE_DATA_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData?name=home`
 export const RID_OF_KEYWORDS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getDisposalKeywordList`
 export const RID_OF_SEARCH_RESULTS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/searchDisposalItems?s`
-export const RID_OF_ITEM_DETAILS_URL= `${ROOT_WORDPRESS_URL}dsny/v1/getDisposalItemPage?name=`
-export const COLLECTION_SCHEDULE_URL= "https://msdwvw-dsndny01.csc.nycnet/DSNYGeoCoder/api/geocoder/DSNYaddresses/"
+export const RID_OF_ITEM_DETAILS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getDisposalItemPage?name=`
+export const COLLECTION_SCHEDULE_URL = "https://msdwvw-dsndny01.csc.nycnet/DSNYGeoCoder/api/geocoder/DSNYaddresses/"
 
 // zzzzexport const MEDIAS_ALL_URL = `${ROOT_WORDPRESS_URL}wp/v2/media/`;
 export const MEDIA_URL = `${ROOT_WORDPRESS_URL}wp/v2/media/id`;
@@ -18,7 +18,7 @@ export const SUB_SECTION_HEADER_URL = constructWpUrl('wp/v2/pagesection?slug=id'
 export const FETCH_PRESS_RELEASE_DETAILS_URL = constructWpUrl('dsny/v1/getPressReleasePage?name=id');
 export const FETCH_PRESS_RELEASE_LIST_URL = constructWpUrl('dsny/v1/getPressReleaseListPage?year=:Year');
 export const FETCH_LANDING_PAGE_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData?name=:category`;
-export const FETCH_CARD_DETAILS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData2?name=:category`;
+export const FETCH_CARD_DETAILS_URL = `${ROOT_WORDPRESS_URL}dsny/v1/getPageData?name=:category`;
 export const FETCH_DROPDOWN_LIST = `${ROOT_WORDPRESS_URL}dsny/v1/getDropdownOptions?cat=:category`;
 export const FETCH_BUREAUS_DETAILS_URL = constructWpUrl('dsny/v1/getPressReleasePage?name=id');
 
