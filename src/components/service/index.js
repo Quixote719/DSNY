ea15a9412e058a52cb159664d26f26861701a572
Daniel Wang <dwang@dsny.nyc.gov>
2017-10-30 14:48:41 -0400

    service page routing

---
+++ src/components/service/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Banner from '../shared/banner';
+
+
+class Service extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Service</div>
+    )
+  }
+}
+
+
+export default Service;
