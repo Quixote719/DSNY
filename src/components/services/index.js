f6e2abf162979024364b9da517b8a35ff299728b
Daniel Wang <dwang@dsny.nyc.gov>
2017-10-30 15:21:30 -0400

    fix for location bcg image on IE

---
+++ src/components/services/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Banner from '../shared/banner';
+
+
+class Services extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Services</div>
+    )
+  }
+}
+
+
+export default Services;
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ import appstyles from './content/styles/application.css';
 import ScrollToTop from './scrollToTop';
 import Home from './components/home';
 import About from './components/about';
-import Service from './components/service'
+import Services from './components/services'
 import Location from './components/about/Location';
 import Leadership from './components/about/Leadership';
 import Bureaus from './components/about/Bureaus';
@@ -67,7 +67,7 @@ ReactDOM.render(
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/about/strategic-plan"} component={StrategicPlan}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/about/strategic-plan/:slug"} component={CardDetailContainer}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/about/foundation/:slug"} component={CardDetailContainer}/>
-              <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/service"} component={Service}/>
+              <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/services"} component={Services}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/resources/press-releases"} component={PressReleaseList}/>
               <Route path={process.env.REACT_APP_SITE_RELATIVE_URL + "/resources/press-releases/:slug"} component={PressReleaseDetail}/>
               <Route path={process.env.REACT_APP_SITE_RELATIVE_URL + "/resources/reports/:slug"} component={CardDetailContainer}/>
