69efbbf37f7798c8caea86a487fab6dedca22a55
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-10-31 14:47:00 -0400

    refactoring

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -126,8 +126,12 @@ const MyForm = withFormik({
     let errors = {}
     if (!values.OrganizationName) {
       errors.OrganizationName = 'Please enter a valid OrganizationName'
-    } else if (/^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(values.OrganizationWebsite)) {
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.OrganizationWebsite)) {
       errors.OrganizationWebsite = 'Invalid website'
+    }
+    if (!values.WillPostCompostRecipientSignage) {
+
+      errors.WillPostCompostRecipientSignage = 'please check this'
     }
     return errors
   },
