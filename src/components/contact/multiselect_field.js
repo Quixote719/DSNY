b75b23e869189352ca4946f45ed1fe250a3415b6
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-11-02 14:49:26 -0400

    updating API object with booleans foe multiselect

--- src/components/contact/multiselect_field.js
+++ src/components/contact/multiselect_field.js
@@ -9,45 +9,25 @@ class FormMultiSelect extends Component {
 
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
-   this.updateObject = this.updateObject.bind(this);
     this.state = {
-      "DeliverOnMonday": false,
-      "DeliverOnTuesday": false,
-      "DeliverOnWednesday": false,
-      "DeliverOnThursday": false,
-      "DeliverOnFriday": false,
-      "DeliverOnSaturday": false
+      options: props.options
     }
   }
 
-  onInputChange(item) {
-    this.setState({option: item.DisplayName});
-    this.props.onChange(this.props.name, item.Id)
+  toggleCheckbox(index) {
+    const {options} = this.state;
+    options[index].Selected = !options[index].Selected;
+    this.props.onMultiSelect(options[index].Name, options[index].Selected)
+    this.setState({options});
   }
 
-
   renderOptions(options) {
-
-    return _.map(options, Item => {
+    return options.map((Item, index) => {
       return (<div key={_.random(0, 200, true)}>
-        <input  type="checkbox"  id={Item.id}  name={Item.Name} value={Item.Name} checked={this.state.value} onClick={event => this.handleChange(event)}/>
+        <input type="checkbox" id={Item.id} name={Item.Name} value={Item.Name} checked={Item.Selected} onChange={this.toggleCheckbox.bind(this, index)}/>
         <span>{Item.DisplayName}</span>
       </div>)
     });
-  }
-
-  handleChange(Item) {
-
-console.log(Item.target.name, Item.target.checked);
-
-  }
-
-  updateObject(Item){
-    this.props.onMultiSelect(Item.target.name , Item.target.checked)
-    var day = Item.target.name
-    this.setState({day : Item.target.checked})
-    console.log(this.state);
   }
 
   render() {
@@ -55,7 +35,7 @@ console.log(Item.target.name, Item.target.checked);
       <Col xs={12}>
         <fieldset>
           <div className='FormMultiSelectTitle'>{this.props.title}</div>
-          <div>{this.renderOptions(this.props.options)}</div>
+          <div>{this.renderOptions(this.state.options)}</div>
         </fieldset>
       </Col>
     </div>);
