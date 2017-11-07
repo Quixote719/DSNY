c25cf9ea234b31f1a194c8add3e917db1c84e6ea
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-11-07 15:29:00 -0500

    making the objects booleans nullable

--- src/reducers/reducer_contact_forms.js
+++ src/reducers/reducer_contact_forms.js
@@ -1,12 +1,26 @@
 import _ from "lodash";
 import * as types from '../constants/ActionTypes';
 
+
+function searchObj (obj, query, replace) {
+
+    for (var key in obj) {
+        var value = obj[key];
+        if (value === query) {
+            obj[key] = replace
+        }
+
+    }
+return obj
+}
+
 export default function cardReducer(state = {}, action) {
   switch (action.type) {
     case types.FETCH_FORM_GET_COMPOST_REQUEST:
+
       return {
         ...state,
-        formObject:action.payload.data
+        formObject:searchObj(action.payload.data, false, null)
       };
     default:
       return state;
