b923c800573c4c5b71f65720ad8aae0d77baa13c
Dinesh Doshi <ddoshi@dsny.nyc.gov>
2017-11-08 18:12:06 -0500

    Making validations dynamic

--- src/components/contact/dropdown_field.js
+++ src/components/contact/dropdown_field.js
@@ -51,7 +51,7 @@ class FormDropdown extends Component {
       <Col className='FormField' xs={12} sm={6} md={6}>
         <fieldset>
           <div className='FormMultiSelectTitle'>{this.props.title}</div>
-          <DropdownButton disabled={this.props.disabled ? this.props.disabled : false}  className='formDropDownButtonText' bsStyle="default" name={this.props.name} onChange={this.props.onChange} title={<div className = "dropDownTitle" > <div className="col-xs-10 dropDownSubTitle">
+          <DropdownButton disabled={this.props.disabled ? this.props.disabled : false}  className={this.props.error?"formDropDownButtonText error":'formDropDownButtonText'} bsStyle="default" name={this.props.name} onChange={this.props.onChange} title={<div className = "dropDownTitle" > <div className="col-xs-10 dropDownSubTitle">
               {this.state.option }
             </div> < div className = "col-xs-2 downArrow" > <i className="fa fa-caret-down "></i> < /div> < /div >} noCaret id="dropdown-no-caret">
             {
