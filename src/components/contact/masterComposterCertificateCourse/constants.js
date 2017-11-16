e6ace4afac83097221d478cea52f647ec662f681
DSNYAD\Siddharth Subramoni <ssubramoni@dsny.nyc.gov>
2017-11-16 09:41:17 -0500

    Creation of MasterComposterCertificateCourseForm

---
+++ src/components/contact/masterComposterCertificateCourse/constants.js
@@ -0,0 +1,94 @@
+export const Titles = {
+  sectionOne: 'YOUR ADDRESS',
+  SectionTwo: 'SELECT BOROUGH FOR TRAINING',
+  SectionThree: 'APPLICATION QUESTIONS (MAXIMUM 1500 CHARACTERS FOR EACH)',
+  SectionFour:'CONTACT INFORMATION',
+  PrefferedLocation:'PREFERED LOCATION',
+  Address:'ADDRESS',
+  AppliedToProgramBefore:'HAVE YOU APPLIED TO THIS PROGRAM BEFORE (OPTIONAL) ',
+  DescribeExperience:'DESCRIBE YOUR COMPOSTING EXPERIENCE',
+  WhyYouWantToBecome:'WHY DO YOU WANT TO BECOME A MASTER COMPOSTER',
+  HowWouldYouUse:'HOW WOULD YOU USE YOUR MASTER COMPOSTER TRAINING',
+  CommunityAffiliations: 'PLEASE LIST ANY COMMUNITY AFFILIATIONS',
+  FirstName:'FIRST NAME',
+  LastName:'LAST NAME',
+  Email:'E-MAIL',
+  ConfirmEmail:'CONFIRM E-MAIL',
+  Phone:'PHONE',
+
+
+  
+}
+
+
+export const formObject = {
+  
+  "$id": "1",
+  "PrefferedLocation": null,
+  "Address":null,
+  "PrefferedLocationType": [
+    {
+      "Id": 1,
+      "Name": "Brooklyn",
+      "DisplayName": "Brooklyn",
+      "Selected": false
+    },
+    {
+      "Id": 2,
+      "Name": "Manhattan",
+      "DisplayName": "Manhattan",
+      "Selected": false
+    },
+    {
+      "Id": 3,
+      "Name": "Queens",
+      "DisplayName": "Queens",
+      "Selected": false
+    },
+    {
+      "Id": 4,
+      "Name": "Bronx",
+      "DisplayName": "Bronx",
+      "Selected": false
+    },
+    {
+      "Id": 5,
+      "Name": "Staten Island",
+      "DisplayName": "Staten Island",
+      "Selected": false
+    }
+  ],
+  "AppliedToProgramBefore": null,
+  "DescribeExperience": null,
+  "WhyYouWantToBecome": null,
+  "HowWouldYouUse": null,
+  "CommunityAffiliations": null,
+  "Id": 0,
+  "SRNumberId": 0,
+  "SRNo": null,
+  "ServiceRequestStatusId": 1,
+  "Source": null,
+  "CreatedDate": "0001-01-01T00:00:00",
+  "FirstName": null,
+  "LastName": null,
+  "Phone": null,
+  "Email": null,
+  "FullName": null,
+  "FullNameLastFirst": null,
+  "AddressAsEntered": null,
+  "HouseNumber": null,
+  "Street": null,
+  "Apartment": null,
+  "Borough": null,
+  "City": null,
+  "State": "NY",
+  "Zip": null,
+  "District": null,
+  "Section": null,
+  "BBL": null,
+  "Latitude": 0,
+  "Longitude": 0,
+  "AddressText": ", NY",
+  "AddressTextOneLine": ", NY"
+  
+}
--- src/index.js
+++ src/index.js
@@ -46,7 +46,7 @@ import TestForm from './components/contact/index';
 import CompostRequest from './components/contact/compostRequest';
 import SiteVisitRequestForm from './components/contact/siteVisitRequest'
 import eventParticipationRequestForm from './components/contact/eventParticipationRequest';
-
+import MasterComposerCertificateCourseForm from './components/contact/masterComposterCertificateCourse';
 import DeadAnimalRemovalRequest from './components/contact/deadAnimalRemovalRequest';
 import OverflowingLitterBasket from './components/contact/overflowingLitterBasket';
 import OrganicsBinReplacement from './components/contact/organicsBinReplacement';
@@ -131,7 +131,7 @@ ReactDOM.render(<Provider store={createStore(reducers, middleware)}>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/siteVisitRequest"} component={SiteVisitRequestForm}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/organicsform"} component={OrganicsForm}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/eventParticipationRequests"} component={eventParticipationRequestForm} />
-
+              <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/mastercompostercourse/"} component={MasterComposerCertificateCourseForm} />
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/complaints/dead-animal-removal-request"} component={DeadAnimalRemovalRequest}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/complaints/overflowing-litter-basket-service-request"} component={OverflowingLitterBasket}/>
               <Route exact path={process.env.REACT_APP_SITE_RELATIVE_URL + "/contact/organics-bin-replacement-request"} component={OrganicsBinReplacement}/>
