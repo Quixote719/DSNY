6a493255506979a10ac6591f50006aafd80f379a
Daniel Wang <dwang@dsny.nyc.gov>
2017-11-18 14:32:15 -0500

    change for forms.

--- src/components/contact/failureStoreReceptacles/constants.js
+++ src/components/contact/failureStoreReceptacles/constants.js
@@ -7,77 +7,202 @@
   "ViolatorAddressAsEntered": "ADDRESS",
   "ViolatorApartment": "FLOOR/SUITE/APT (OPTIONAL)",
   "TimeOfOccurrence": "TIME OF OCCURRENCE",
-  "": "WHEN ARE THE RECEPTACLES BEING PLACED OUT?",
+  "PlaceTime": "WHEN ARE THE RECEPTACLES BEING PLACED OUT?",
+  "FromHourOfDayId": "TIME OF OCCURRENCE",
   "FirstName": "FIRST NAME",
   "LastName": "LAST NAME",
   "Email": "E-MAIL",
   "ConfirmEmail": "CONFIRM E-MAIL",
   "Phone": "PHONE",
   "IsAnonymous": "I would like to remain anonymous",
-
-
-
-
-
-
-  // "RemovedMaterialsDescription": "DESCRIBE MATERIALS REMOVED (OPTIONAL)",
-  // "IncidentDate": "INCIDENT DATE (OPTIONAL)",
-  // "IncidentTime": "INCIDENT TIME (OPTIONAL)",
-  // "VehicleLicensePlateNumber": "VEHICLE LICENSE PLATE# (OPTIONAL)",
-  // "VehicleState": "LICENSED STATE (OPTIONAL)",
-  // "VehicleMake": "VEHICLE MAKE (OPTIONAL)",
-  // "VehicleModel": "VEHICLE MODEL (OPTIONAL)",
-  // "VehicleColor": "VEHICLE COLOR (OPTIONAL)",
-  // "VehicleType": "VEHICLE TYPE (OPTIONAL)",
-  // "AdditionalVehicleInfo": "ADDTIONAL VEHICLE DESCRIPTION (DENTS, COMPANY NAME, ETC.) (OPTIONAL)",
-  // "FirstName": "FIRST NAME",
-  // "LastName": "LAST NAME",
-  // "Email": "E-MAIL",
-  // "ConfirmEmail": "CONFIRM E-MAIL",
-  // "Phone": "PHONE",
-  // "IsAnonymous": "I would like to remain anonymous",
-  // "IsVehicleCommercial": "Commercial License"
 }
 
 export const formObject = {
-    "ViolatorName": null,
-    "ViolatorAddressAsEntered": null,
-    "ViolatorHouseNumber": null,
-    "ViolatorStreet": null,
-    "ViolatorApartment": null,
-    "ViolatorBorough": null,
-    "ViolatorCity": null,
-    "ViolatorState": null,
-    "ViolatorZip": null,
-    "TimeOfOccurrence": null,
-    "PreCollection": false,
-    "PostCollection": false,
-    "IsAnonymous": false,
-    "Id": 0,
-    "SRNumberId": 0,
-    "SRNo": null,
-    "ServiceRequestStatusId": 1,
-    "Source": null,
-    "CreatedDate": "0001-01-01T00:00:00",
-    "FirstName": null,
-    "LastName": null,
-    "Phone": null,
-    "Email": null,
-    "FullName": null,
-    "FullNameLastFirst": null,
-    "AddressAsEntered": null,
-    "HouseNumber": null,
-    "Street": null,
-    "Apartment": null,
-    "Borough": null,
-    "City": null,
-    "State": "NY",
-    "Zip": null,
-    "District": null,
-    "Section": null,
-    "BBL": null,
-    "Latitude": 0,
-    "Longitude": 0,
-    "AddressText": ", NY",
-    "AddressTextOneLine": ", NY"
+  "ViolatorName": null,
+  "ViolatorAddressAsEntered": null,
+  "ViolatorHouseNumber": null,
+  "ViolatorStreet": null,
+  "ViolatorApartment": null,
+  "ViolatorBorough": null,
+  "ViolatorCity": null,
+  "ViolatorState": null,
+  "ViolatorZip": null,
+  "TimeOfOccurrence": null,
+  "ReceptaclePlacedOut": null,
+  "IsAnonymous": false,
+  "Id": 0,
+  "SRNumberId": 0,
+  "SRNo": null,
+  "ServiceRequestStatusId": 1,
+  "Source": null,
+  "CreatedDate": "0001-01-01T00:00:00",
+  "FirstName": null,
+  "LastName": null,
+  "Phone": null,
+  "Email": null,
+  "FullName": null,
+  "FullNameLastFirst": null,
+  "AddressAsEntered": null,
+  "HouseNumber": null,
+  "Street": null,
+  "Apartment": null,
+  "Borough": null,
+  "City": null,
+  "State": "NY",
+  "Zip": null,
+  "District": null,
+  "Section": null,
+  "BBL": null,
+  "Latitude": 0,
+  "Longitude": 0,
+  "AddressText": ", NY",
+  "AddressTextOneLine": ", NY",
+  "FromHourOfDayId": 0,
+  "SelectedFromHourOfDay": null,
+  "FromHoursOfDay": [
+    {
+      "Id": 1,
+      "Name": "OneAm",
+      "DisplayName": "01:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 13,
+      "Name": "OnePm",
+      "DisplayName": "01:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 2,
+      "Name": "TwoAm",
+      "DisplayName": "02:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 14,
+      "Name": "TwoPm",
+      "DisplayName": "02:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 3,
+      "Name": "ThreeAm",
+      "DisplayName": "03:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 15,
+      "Name": "ThreePm",
+      "DisplayName": "03:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 4,
+      "Name": "FourAm",
+      "DisplayName": "04:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 16,
+      "Name": "FourPm",
+      "DisplayName": "04:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 5,
+      "Name": "FiveAm",
+      "DisplayName": "05:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 17,
+      "Name": "FivePm",
+      "DisplayName": "05:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 6,
+      "Name": "SixAm",
+      "DisplayName": "06:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 18,
+      "Name": "SixPm",
+      "DisplayName": "06:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 7,
+      "Name": "SevenAm",
+      "DisplayName": "07:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 19,
+      "Name": "SevenPm",
+      "DisplayName": "07:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 8,
+      "Name": "EightAm",
+      "DisplayName": "08:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 20,
+      "Name": "EightPm",
+      "DisplayName": "08:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 9,
+      "Name": "NineAm",
+      "DisplayName": "09:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 21,
+      "Name": "NinePm",
+      "DisplayName": "09:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 10,
+      "Name": "TenAm",
+      "DisplayName": "10:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 22,
+      "Name": "TenPm",
+      "DisplayName": "10:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 11,
+      "Name": "ElevenAm",
+      "DisplayName": "11:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 23,
+      "Name": "ElevenPm",
+      "DisplayName": "11:00 PM",
+      "Selected": false
+    },
+    {
+      "Id": 0,
+      "Name": "TwelveAm",
+      "DisplayName": "12:00 AM",
+      "Selected": false
+    },
+    {
+      "Id": 12,
+      "Name": "TwelvePm",
+      "DisplayName": "12:00 PM",
+      "Selected": false
+    }
+  ]
 }
