7c349118ab2e4eb80d9856b6098be6da104d95c6
Daniel Wang <dwang@dsny.nyc.gov>
2017-11-20 17:05:52 -0500

    push for redesigned breadscrumb and banner

--- src/components/shared/banner.js
+++ src/components/shared/banner.js
@@ -6,7 +6,7 @@ class Banner extends Component {
   render() {
 
     return (
-      <div className="GBanner">
+      <div className="GBanner MainPageBanner">
         <div className="GTexture">
           <div className="GBannerTitle">{this.props.text.title}</div>
           <div className="GBannerContent" dangerouslySetInnerHTML={{__html: this.props.text.content}}></div>
