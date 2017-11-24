048705aeb990600b1a9ba71fdf407eddc0c63f36
qzhang <qzhang@dsny.nyc.gov>
2017-11-24 14:00:35 -0500

    Handle external link

---
+++ src/components/shared/titleCardExternalLink.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react";
+import { Link } from 'react-router-dom';
+import '../../content/styles/dsnyCard.css';
+
+class TitleCardExternalLink extends Component {
+  render() {
+    const styles={
+      without_border:{
+        'height': '90px',
+        'display':'inline-block',
+      },
+      with_border:{
+        'height': '90px',
+        'display':'inline-block',
+        'border': '1px solid #7CC04B'
+      }
+    }
+    let CardType = styles.without_border;
+    switch(this.props.type){
+      case '1':{
+        CardType = styles.without_border;
+        break;
+      }
+      case '2':{
+        CardType = styles.with_border;
+        break;
+      }
+      default:{
+        break;
+      }
+    }
+
+    let link = this.props.link;
+    console.log(link);
+    return (
+        <Link to={link}>
+            <div style = {CardType} className='CardTitle'>
+                {this.props.title}
+            </div>
+        </Link>
+    );
+  };
+};
+
+
+export default TitleCardExternalLink;
