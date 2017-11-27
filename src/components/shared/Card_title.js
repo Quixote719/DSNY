44363e0086ab7b5b53553116b413e8b02fa6a4a0
DSNYAD\Siddharth Subramoni <ssubramoni@dsny.nyc.gov>
2017-11-27 12:44:29 -0500

    Modified number of lines to be displayed before Truncating for Card Titles......

--- src/components/shared/Card_title.js
+++ src/components/shared/Card_title.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import {Col, Clearfix} from 'react-bootstrap';
 import '../../content/styles/card.css';
 import {Route, Redirect} from "react-router-dom";
+import Truncate from 'react-truncate';
+import Parser from 'html-react-parser';
+
 class CardTitle extends Component {
 
   render() {
@@ -10,7 +13,7 @@ class CardTitle extends Component {
       <Col xs={12} sm={6} md={3}>
       <div className={this.props.className}>
         <div className='subSectioncardT' onClick={this.login}>
-            <div className='CardTitleTextEmptyBody'>{this.props.title}</div>
+            <div className='CardTitleTextEmptyBody'><Truncate lines={2}> {Parser(this.props.title)} </Truncate> </div>
         </div>
       </div>  
       </Col>
