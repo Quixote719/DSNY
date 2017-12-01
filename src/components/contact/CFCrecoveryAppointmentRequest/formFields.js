a49c317f11110215ebfc8580edd7e8f488356793
yeswanth varma <ykanumuri@dsny.nyc.gov>
2017-12-01 10:03:42 -0500

    cfc recycling location

--- src/components/contact/CFCrecoveryAppointmentRequest/formFields.js
+++ src/components/contact/CFCrecoveryAppointmentRequest/formFields.js
@@ -12,6 +12,7 @@ import {Field} from 'formik'
 import {Titles} from './constants'
 import '../../../content/styles/compostRequest.css';
 import FormAddressAutocomplete from '../formAddressAutocomplete'
+import moment from 'moment';
 import {Col} from 'react-bootstrap';
 const DisplayFormikState = props => <div style={{
 		margin: '1rem 0'
@@ -33,15 +34,22 @@ const DisplayFormikState = props => <div style={{
   </div>;
 
 const EwastePickUpRequestFormElements = (props) => {
-	const {values, setFieldValue} = props;
+	const {values, setFieldValue, Dates,  geoCoderAddressResult } = props;
+
+	if (Dates && geoCoderAddressResult){
+     values.Dates = Dates;
+		 values.AppointmentDate = moment(Dates[0].StartDate);
+		 values.SectionAndSubsection = geoCoderAddressResult.sanitationCollectionSchedulingSectionAndSubsection;
+		 values.Frequency = "Tuesday Thursday Saturday"
+	}
 
 	return (<fieldset className='disabledContactForm' disabled={values.editMode}>
 		<FormHeader title='Online Service Request Form'/>
 		<FormSectionHeader title={Titles.sectionOne}/>
 		<div><FormAddressAutocomplete name="AddressAsEntered"  {...props}   value="" disabled={values.editMode}/></div>
 		<FormSectionHeader title={Titles.sectionTwo}/>
-		<Field component={DropdownInput} required name="RecyclingLocation" {...props} onChange={setFieldValue} options={values.CompostSitePermittingOrganizations} disabled={values.editMode}/>
-		<Field component={DateTimePickerInput} required name="AppointmentDate" {...props} onChange={setFieldValue}/>
+		<Field component={DropdownInput} required name="RecyclingLocation" options={geoCoderAddressResult ? geoCoderAddressResult.pickupStreets :[]} {...props} onChange={setFieldValue} disabled={values.editMode}/>
+		<Field component={DateTimePickerInput} value={values.AppointmentDate ? values.AppointmentDate : ''} Dates={values.Dates} required name="AppointmentDate" {...props} onChange={setFieldValue}/>
 		<Field component={Nstepper} name="Appliances" header='APPLIANCES' tableHeader='Electronic Category' {...props} required="required" categories={values.categories} disabled={values.editMode} onAppend={setFieldValue}/>
 		<FormSectionHeader title={Titles.sectionThree}/>
 		<Field component={TextInput} name="FirstName" {...props} required="required"/>
