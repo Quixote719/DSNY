f04f5984566c5fd621b5ee36f00a39fb4e9ab0ae
Daniel Wang <dwang@dsny.nyc.gov>
2017-12-08 15:07:55 -0500

    commit for form

--- src/components/contact/organicsCollectionApplication/index.js
+++ src/components/contact/organicsCollectionApplication/index.js
@@ -206,12 +206,20 @@ class OrganicsCollectionApplication extends Component {
   }
 
    render() {
-        const { error, success, geoCoderAddressResult, isAddressValidated} = this.props;
 
-        if(success !== undefined) {
+        const { error, success, geoCoderAddressResult, isAddressValidated} = this.props;
+        console.log('success?');
+        console.log(error);
+        if(error){
+
+        }
+        if(success!==undefined) {
+          if(success){
+
+          }
           console.log('Thank you');
           console.log(success);
-          return displayThankYouPage(`<div><div class='thankyoulable'>THANK YOU</div><div class='thankyoubody'><p>The Service Request number is</p><p class='SRNumberThankYou'>${success.SRNo}</p><p>Use this number when you check the status of your request.</p><p>You will also receive an email with this information. To check the status of this request please visit the DSNY Website Contact page. To reschedule or cancel your request please call 311.</p><p><b>Where to leave your E-Waste items?</b></p><p>Place your E-Waste items at the curb for DSNY collection after 4 PM the day before your appointment date. DSNY will NOT come inside your house or ring your bell; items to be picked up MUST BE AT THE CURB.</p></div></div>`)
+          return displayThankYouPage(`<div><div class='thankyoulable'>THANK YOU</div><div class='thankyoubody'><p>The Service Request number is</p><p class='SRNumberThankYou'>${success.SRNo}</p><p>Use this number when you check the status of your request.</p><p>You will also receive an email with this information. To check the status of this request please visit the DSNY Website Contact page. To reschedule or cancel your request please call 311.</p></div></div>`)
         }
 
         if (FormObject && FormObject !== undefined) {
